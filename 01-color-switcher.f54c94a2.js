860b337709987b8a3f3bafa613d403914fbee2aa
rostiq <106112250+rostiq@users.noreply.github.com>
2022-11-07 23:16:13 +0000

    Deploying to gh-pages from @ rostiq/goit-js-hw-09@a5818a1b4dcafefc856bdc918203d5939acfb19e 🚀

---
+++ 01-color-switcher.f54c94a2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.setAttribute("disabled","disabled"),intervalID=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(intervalID)}))}();
+//# sourceMappingURL=01-color-switcher.f54c94a2.js.map
